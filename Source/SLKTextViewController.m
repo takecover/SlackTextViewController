d2617998a675ea2d417f9435110699fb7df2f651
Tyler <tyler@noyesrun.com>
2017-10-30 23:41:55 +0900

    my temp fix

--- Source/SLKTextViewController.m
+++ Source/SLKTextViewController.m
@@ -456,6 +456,8 @@ CGFloat const SLKAutoCompletionViewDefaultHeight = 140.0;
     scrollViewHeight -= self.textInputbarHC.constant;
     scrollViewHeight -= self.autoCompletionViewHC.constant;
     scrollViewHeight -= self.typingIndicatorViewHC.constant;
+
+    scrollViewHeight -= [self slk_appropriateBottomMargin];
     
     if (scrollViewHeight < 0) return 0;
     else return scrollViewHeight;
